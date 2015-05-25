a2a8aa9f0e385231ec792c56c5a45565c8a2a854
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2015-05-25 15:14:13 +0100

    first version

---
+++ index.js
@@ -0,0 +1,28 @@
+(function () {
+
+  "use strict";
+
+  angular.module('eeg-angular', [])
+  .directive('eeg', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        // two way bind 
+        // we expect the graph to be contain
+        // nodes: []
+        // links: []
+        // option: {}
+        // nodes and links and option format same as in Eg
+        graph: '='
+      },
+      template: '<div></div>',
+      link: function ($scope, element, attrs) {
+        var g = new Eg(element, {} || $scope.graph.options);
+        g.addNodes($scope.graph.nodes);
+        g.addLinks($scope.graph.links);
+      }
+    };
+  });  
+
+})();
